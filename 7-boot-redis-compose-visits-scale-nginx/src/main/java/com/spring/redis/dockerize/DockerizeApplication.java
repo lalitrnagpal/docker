ead4df0208ee36e192bdb4115637dc43e651ce1b
Lalit Nagpal <lalit2801@gmail.com>
2020-12-04 23:27:12 +0530

    Docker Compose Scaling Example Nginx

---
+++ 7-boot-redis-compose-visits-scale-nginx/src/main/java/com/spring/redis/dockerize/DockerizeApplication.java
@@ -0,0 +1,14 @@
+package com.spring.redis.dockerize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerizeApplication.class, args);
+		System.out.println("Hello Dockerized Spring Boot!");
+	}
+
+}
---
+++ 7-boot-redis-compose-visits-scale-nginx/src/test/java/com/spring/redis/dockerize/DockerizeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.redis.dockerize;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerizeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
