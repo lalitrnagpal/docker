acf089d6b0b7b84d9e47cf29dc24120d3c372213
Lalit Nagpal <lalit2801@gmail.com>
2020-12-10 16:09:37 +0530

    Docker Microservice API Gateway Example

---
+++ 20-currency-microservice-apigateway/20-currency-conversion-service/src/main/java/com/boot/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,16 @@
+package com.boot.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.boot.microservices.currencyconversionservice.resource")
+@EnableDiscoveryClient
+public class CurrencyConversionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+}
